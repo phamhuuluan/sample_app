ecc984645998f135f79978e9a01109610779c7ef
phamhuuluan <luanphamm15@gmail.com>
2019-09-30 14:56:19 -0400

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
-
-  def hello
-    render html: "hello,world!"
-  end
+    before_action :set_locale
+    def set_locale
+        I18n.locale = params[:locale] || I18n.default_locale
+        I18n.locale = I18n.available_locales.include?(locale) ?
+         locale : I18n.default_locale
+    end
+    def default_url_options
+        {locale: I18n.locale}
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-   root 'application#hello'
+  root :to => "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  scope "(:locale)", locale: /en|vi/ do
+ end
 end
