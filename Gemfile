afd52dc545f9f05cae89be530868c665fb1db857
phamhuuluan <luanphamm15@gmail.com>
2019-10-04 08:35:56 -0400

    Chapter 6: modeling-user

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.6.3"
 
 gem "rails", "~> 6.0.0"
 gem "bootstrap-sass", "3.3.7"
+gem "bcrypt","3.1.12"
 gem "rails-i18n"
 gem "config"
 gem "sqlite3", "~> 1.4"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Settings.models.user.email_regex
+  before_save :downcase
+  validraates :name, presence: true, length: {maximum: Settings.models.user.namevalidates}
+  validates :email, presence: true, length: {maximum: Settings.models.user.emaillvalidates},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum:  Settings.models.user.passvalidates}
+
+  has_secure_password
+
+  private
+  def downcase
+    email.downcase!
+  end
 end
