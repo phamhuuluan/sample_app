dfde0802c5b8a2e58636b53bccb1cb810edfa652
phamhuuluan <luanphamm15@gmail.com>
2019-11-15 10:58:10 -0500

    following -users-ver2

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,14 @@
+class FollowersController < ApplicationController
+  def index
+    @title = t ".title"
+    @user = User.find_by id: params[:id]
+   
+    if @user
+      @users = @user.followers.page(params[:page]).per Settings.users.index.page
+      render "users/show_follow"
+    else
+      flash[:danger] = t ".not_found"
+      redirect_to request.referrer || root_path
+    end
+  end
+end
